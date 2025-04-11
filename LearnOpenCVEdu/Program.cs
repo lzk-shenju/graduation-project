715cc28e5e5507fcecbcbfbebac77d16555067ee
神居 <sviycz387@163.com>
2025-04-11 17:37:31 +0800

    添加项目文件。

---
+++ LearnOpenCVEdu/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
+using System.Windows.Forms;
+
+namespace LearnOpenCVEdu
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            CheckAdmin();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMain());
+        }
+
+        /// <summary>
+        /// 开机检查是否以管理员身份运行
+        /// </summary>
+        private static void CheckAdmin()
+        {
+            var wi = WindowsIdentity.GetCurrent();
+            var wp = new WindowsPrincipal(wi);
+
+            bool runAsAdmin = wp.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!runAsAdmin)
+            {
+                var processInfo = new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase);
+
+                processInfo.UseShellExecute = true;
+                processInfo.Verb = "runas";
+
+                try
+                {
+                    Process.Start(processInfo);
+                }
+                catch (Exception)
+                {
+                    //MessageBox.Show(ex.Message);
+                }
+
+                Environment.Exit(0);
+            }
+        }
+    }
+}
